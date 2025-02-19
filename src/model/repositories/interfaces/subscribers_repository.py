cffc45742d73bd196aeedab904f04f456de0842d
Renato T M Lima <renato.matura@gmail.com>
2025-02-18 21:45:12 -0500

    feat: added interfaces

---
+++ src/model/repositories/interfaces/subscribers_repository.py
@@ -0,0 +1,9 @@
+from src.model.entities.inscritos import Inscritos
+from abc import ABC, abstractmethod
+
+class SubscribersRepositoryInterface(ABC):
+    @abstractmethod
+    def insert(self, subscriber_info: dict) -> None: pass
+            
+    @abstractmethod
+    def select_subscriber(self, email: str, event_id: int) -> Inscritos: pass
